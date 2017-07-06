632aff4df994082e0d5ed3af2e9b63ac8e55b8d8
Nils <Nils.Hensch@gmail.com>
2017-07-06 20:02:48 +0200

    Unity Projekt hinzugefügt
    Alles bisherigen Dateien hinzugefügt

---
+++ VR_Car/Assets/scripts/characterController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class characterController : MonoBehaviour {
+
+	public float speed = 10.0f;
+    public float roationspeed = 10.0f;
+
+	// Use this for initialization
+	void Start () {
+		Cursor.lockState = CursorLockMode.Locked;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float translation = Input.GetAxis ("Vertical") * speed;
+		float rotation = Input.GetAxis ("Horizontal") * roationspeed;
+		translation *= Time.deltaTime;
+		rotation *= Time.deltaTime;
+
+		transform.Translate (0, 0, translation);
+        transform.Rotate (0, rotation, 0);
+
+		if (Input.GetKeyDown ("escape"))
+			Cursor.lockState = CursorLockMode.None;
+	}
+}
